c9ac62db73f92025e0fd05cb22ce7b2abcc206b6
mikek182 <marcosvinicius@geekwork.com.br>
2025-05-08 13:58:34 -0300

    find

---
+++ Instance methods/find.js
@@ -0,0 +1,6 @@
+// O método find() retorna o valor do primeiro elemento do array que satisfizer a função de teste provida. Caso contrario, undefined é retornado.
+
+const array1 = [5, 12, 8, 130, 44];
+
+const found = array1.find((element) => element > 10);
+console.log(found); // expected output: 12
