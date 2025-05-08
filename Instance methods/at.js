c9dbe7e4288785f592cca6ae732444f4f986d2bb
mikek182 <marcosvinicius@geekwork.com.br>
2025-05-08 12:58:14 -0300

    at

--- Instance methods/at.js
+++ Instance methods/at.js
@@ -1,4 +1,8 @@
+// O método at() recebe um valor inteiro e retorna o item referente ao index dele, 
+// permitindo valores positivos ou negativos. Valores negativos contam apartir do último item do array.
+
 const array1 = [5, 12, 8, 130, 44];
+const array2 = ['a', 'b', 'c', 'd'];
 
 let index = 2;
 
@@ -9,3 +13,7 @@ index = -2;
 
 console.log(`An index of ${index} returns ${array1.at(index)}`);
 // Expected output: "An index of -2 returns 130"
+
+
+index = 3
+console.log(`An index of ${index} returns ${array2.at(index)}`);
--- Instance methods/concat.js
+++ Instance methods/concat.js
@@ -1,6 +1,7 @@
 const array1 = ['a', 'b', 'c'];
 const array2 = ['d', 'e', 'f'];
-const array3 = array1.concat(array2);
+const array3 = ['g', 'h', 'i'];
+const array4 = array1.concat(array2, array3);
 
-console.log(array3);
+console.log(array4);
 // Expected output: Array ["a", "b", "c", "d", "e", "f"]
