9d6124b002b3ca8da2f4e2a07726a4655d996c0e
mikek182 <marcosvinicius@geekwork.com.br>
2025-05-16 10:29:24 -0300

    find (is prime)

--- Instance methods/find.js
+++ Instance methods/find.js
@@ -21,3 +21,20 @@ const found3 = array3.find(element => element.nome === null);
 
 console.log(found3);
 // Saída: { id: 2, nome: null }
+
+// Encontrar um número primo em um array
+// O exemplo a seguir encontra um elemento dentro da array que é número primo 
+// (ou retorna undefined se não houverem números primos).
+
+function isPrime(element, index, array) {
+  var start = 2;
+  while (start <= Math.sqrt(element)) {
+    if (element % start++ < 1) {
+      return false;
+    }
+  }
+  return element > 1;
+}
+
+console.log([4, 6, 8, 12].find(isPrime)); // undefined, not found
+console.log([4, 5, 8, 12].find(isPrime)); // 5
