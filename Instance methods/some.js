677cf5d1e89b9421626aff0487e5b9373352c1fc
mikek182 <marcosvinicius@geekwork.com.br>
2025-05-16 12:40:18 -0300

    some

---
+++ Instance methods/some.js
@@ -0,0 +1,11 @@
+// O some()método de Arrayinstâncias testa se pelo menos um elemento no array passa no teste 
+// implementado pela função fornecida. Retorna verdadeiro se, no array, encontrar um elemento para o qual a função fornecida retorna verdadeiro; caso contrário, retorna falso. 
+// Ele não modifica o array.
+
+const array = [1, 2, 3, 4, 5];
+
+// Checks whether an element is even
+const even = (element) => element % 2 === 0;
+
+console.log(array.some(even));
+// Expected output: true
