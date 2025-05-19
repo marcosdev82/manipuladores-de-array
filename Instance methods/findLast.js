2a6eef2f593e34fec1dfaf96fed031798e026894
mikek182 <marcosvinicius@geekwork.com.br>
2025-05-19 17:00:37 -0300

    findLast

--- Instance methods/findLast.js
+++ Instance methods/findLast.js
@@ -8,3 +8,21 @@ const found = array1.findLast((element) => element > 45);
 
 console.log(found);
 // Expected output: 130
+
+// Encontre o último objeto em uma matriz que corresponda às propriedades do elemento
+// Este exemplo mostra como você pode criar um teste com base nas propriedades dos elementos da matriz.
+
+const inventory = [
+  { name: "apples", quantity: 2 },
+  { name: "bananas", quantity: 0 },
+  { name: "fish", quantity: 1 },
+  { name: "cherries", quantity: 5 },
+];
+
+// return true inventory stock is low
+function isNotEnough(item) {
+  return item.quantity < 2;
+}
+
+console.log(inventory.findLast(isNotEnough));
+// { name: "fish", quantity: 1 }
