029513c792e481dff884d66aa6104c670a71d174
mikek182 <marcosvinicius@geekwork.com.br>
2025-05-19 17:04:12 -0300

    findLast

--- Instance methods/findLast.js
+++ Instance methods/findLast.js
@@ -13,16 +13,34 @@ console.log(found);
 // Este exemplo mostra como você pode criar um teste com base nas propriedades dos elementos da matriz.
 
 const inventory = [
-  { name: "apples", quantity: 2 },
-  { name: "bananas", quantity: 0 },
-  { name: "fish", quantity: 1 },
-  { name: "cherries", quantity: 5 },
+    { name: "apples", quantity: 2 },
+    { name: "bananas", quantity: 0 },
+    { name: "fish", quantity: 1 },
+    { name: "cherries", quantity: 5 },
 ];
 
 // return true inventory stock is low
 function isNotEnough(item) {
-  return item.quantity < 2;
+    return item.quantity < 2;
 }
 
 console.log(inventory.findLast(isNotEnough));
 // { name: "fish", quantity: 1 }
+
+
+// Encontre o útimo primo em uma matriz
+
+function isPrime(element) {
+    if (element % 2 === 0 || element < 2) {
+        return false;
+    }
+    for (let factor = 3; factor <= Math.sqrt(element); factor += 2) {
+        if (element % factor === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log([4, 6, 8, 12].findLast(isPrime)); // undefined, not found
+console.log([4, 5, 7, 8, 9, 11, 12].findLast(isPrime)); // 11
