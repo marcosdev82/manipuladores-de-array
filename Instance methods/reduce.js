091b24da140dc67916dbee696f3a6a5ce46c622a
mikek182 <marcosvinicius@geekwork.com.br>
2025-05-22 10:42:38 -0300

    Reduce

---
+++ Instance methods/reduce.js
@@ -0,0 +1,14 @@
+// O método reduce() executa uma função reducer (fornecida por você) para cada elemento do array, 
+// resultando num único valor de retorno.
+
+const array1 = [1, 2, 3, 4];
+
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;
+const sumWithInitial = array1.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue,
+);
+
+console.log(sumWithInitial);
+// Expected output: 10
