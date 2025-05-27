69bf68d6ee644353fee608a48d5fe16619753c42
mikek182 <marcosvinicius@geekwork.com.br>
2025-05-27 16:57:02 -0300

    ReduceRight

---
+++ Instance methods/reduceRight.js
@@ -0,0 +1,11 @@
+// O método reduceRight() aplica à uma função um acumulador e cada valor do array (da direita para esquerda) é reduzido para um valor único.
+
+const acumulador = [0, 1, 2, 3, 4].reduceRight(function (
+    previousValue,  
+    currentValue, 
+    array,
+) {
+    return previousValue + currentValue; 
+}, 10);
+
+console.log(acumulador); // 20
