dd096fdf2bce1cec3ea337922485ea9fafd20fd4
mikek182 <marcosvinicius@geekwork.com.br>
2025-05-27 11:19:16 -0300

    array construct

--- Static methods/array-construct-multiple-parameters.js
+++ Static methods/array-construct-multiple-parameters.js
@@ -1,5 +1,9 @@
 // Construtor de array com vários parâmetros
-const fruits = new Array("Apple", "Banana");
+const fruits = new Array("Apple", "Banana", "Laranja");
 
 console.log(fruits.length); // 2
 console.log(fruits[0]); // "Apple"
+console.log(fruits[2]); // "Laranja"
+console.log(fruits[3]); // undefined
+console.log(fruits); // [ 'Apple', 'Banana', 'Laranja' ]
+console.log(fruits instanceof Array); // true
--- Static methods/array-of.js
+++ Static methods/array-of.js
@@ -4,11 +4,11 @@ console.log(Array.of());
 
 // Diferenaça entre array of e array construtor
 
-// A diferença entre Array.of()e o Array()construtor está no tratamento de argumentos únicos: Array.of(7) cria uma matriz com um único elemento
-// , 7, enquanto Array(7) cria uma matriz vazia com uma lengthpropriedade de 7. (Isso implica uma matriz de 7 slots vazios, não slots com undefinedvalores reais.)
+// A diferença entre Array.of()e o Array() construtor está no tratamento de argumentos únicos: Array.of(7) cria uma matriz com um único elemento
+// , 7, enquanto Array(7) cria uma matriz vazia com uma length propriedade de 7. (Isso implica uma matriz de 7 slots vazios, não slots com undefined valores reais.)
 
-Array.of(7); // [7]
-Array(7); // Array com 7 slotes vazil.
+console.log(Array.of(7)); // [7]
+console.log(Array(7)); // Array com 7 slotes vazil.
 
-Array.of(1, 2, 3); // [1, 2, 3]
-Array(1, 2, 3); // [1, 2, 3]
+console.log(Array.of(1, 2, 3)); // [1, 2, 3]
+console.log(Array(1, 2, 3)); // [1, 2, 3]
