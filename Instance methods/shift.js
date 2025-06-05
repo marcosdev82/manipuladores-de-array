9effe1ae44112dc6039a24043ce6dfa341f9fdd1
mikek182 <marcosvinicius@geekwork.com.br>
2025-06-05 14:13:05 -0300

    shift

---
+++ Instance methods/shift.js
@@ -0,0 +1,12 @@
+// O método shift() remove o primeiro elemento de um array e retorna esse elemento. 
+// Este método muda o tamanho do array.
+
+const array1 = [1, 2, 3];
+
+const firstElement = array1.shift();
+
+console.log(array1);
+// Expected output: Array [2, 3]
+
+console.log(firstElement);
+// Expected output: 1
