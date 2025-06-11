6511c784e9478acf39da2ad4cbee116ee7acb672
mikek182 <marcosvinicius@geekwork.com.br>
2025-06-11 17:26:08 -0300

    exercicio 1

--- Instance methods/exercícios/exercicio-1.js
+++ Instance methods/exercícios/exercicio-1.js
@@ -8,39 +8,21 @@
 let alunosTurmaA = ["Ana", "Bruno", "Carlos"];
 let alunosTurmaB = ["Diana", "Eduardo", "Fernanda"];
 
-// let todosAlunos = alunosTurmaA.concat(alunosTurmaB)
-// console.log(todosAlunos) 
-
-// let novoAluno = todosAlunos.push('marcos')
-// console.log(todosAlunos) 
-// console.log(todosAlunos.at(0))
-// console.log(todosAlunos[0])
-// console.log(todosAlunos.at(-1))
-// console.log(todosAlunos[todosAlunos.length - 1])
-
-// for(let indice in todosAlunos) {
-//   console.log(`indice: ${indice}, nome: ${todosAlunos[indice]}`)
-// }
-// for(let [index, nome] of Object.entries(todosAlunos)) {
-//   console.log(`indice: ${index}, nome: ${nome}`)
-// }
-
 // 1. Concatenar as duas listas em uma única lista.
-const arrAlunos = alunosTurmaA.concat(alunosTurmaB)
-console.log(arrAlunos)
+const allAlunos = alunosTurmaA.concat(alunosTurmaB)
+console.log(allAlunos)
 
 // 2. Adicionar um novo aluno ao final da lista.
-const arrAlunosModificados = arrAlunos.push('Marcos')
-console.log(arrAlunos)
+const newAlunos = allAlunos.push('Marcos')
+console.log(newAlunos, allAlunos)
 
 // 3. Exibir o primeiro e o último aluno da nova lista utilizando `at()`.
-console.log(arrAlunos.at(0))
-console.log(arrAlunos.at(-1))
+console.log(allAlunos.at(0))
+console.log(allAlunos.at(-1))
 
 // 4. Iterar sobre a lista usando `for..in`, exibindo o índice e o nome do aluno.
-for(let index in arrAlunos) {
-  console.log(arrAlunos[index])
+for (let aluno in allAlunos) {
+  console.log(`indice: ${aluno}, nome: ${allAlunos[aluno]}` )
 }
-
 // 5. Utilizar `Object.entries()` para transformar o array em pares `[índice, nome]` e imprimir cada par.
-console.log(Object.entries(arrAlunos))
+console.log(Object.entries(allAlunos))
