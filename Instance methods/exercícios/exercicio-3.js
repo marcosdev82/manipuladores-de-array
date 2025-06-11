0b2b0e3d7023de2624381ac1d76ae6bfd17ef491
mikek182 <marcosvinicius@geekwork.com.br>
2025-06-11 17:45:01 -0300

    exercicio 3

--- Instance methods/exercícios/exercicio-3.js
+++ Instance methods/exercícios/exercicio-3.js
@@ -10,51 +10,29 @@
 
 // const nomes = ["Carlos", "Ana", "Beatriz", "Daniel", "Eduardo"];
 
-// // 1. Exibir cada nome com seu índice.
-// nomes.forEach((nome, indice) => console.log(`Nome: ${nome}, Indice: ${indice}`))
-
-// // 2. Verificar se todos os nomes possuem mais de 3 letras.
-// // let maiorTres = nomes.filter((nome) => nome.length > 3 )
-// let maiorTres = nomes.every((nome) => nome.length > 3 )
-
-// // 4. Exibir o primeiro e o último nome da lista usando `.at()`.
-// console.log(nomes.at(0))
-// console.log(nomes.at(-1))
-
-// // 5. Mostrar uma substring com os 3 primeiros caracteres de cada nome.
-// let subStr = nomes.map((nome) => nome.substr(0,3))
-// console.log(subStr)
-
-// // 6. Reverter a lista e exibi-la.
-// console.log(nomes.reverse())
-
-// // 7. Exibir cada nome usando `for...of`
-// for (let key in nomes) {
-//     console.log(nomes[key])
-// }
-
-// // 3. Ordenar os nomes em ordem alfabética.
-// nomes.sort()
-// console.log(nomes);
-
 const nomes = ["Carlos", "Ana", "Beatriz", "Daniel", "Eduardo"];
+
 // 1. Exibir cada nome com seu índice usando forEach.
-nomes.forEach((value, index) => {
-    console.log(`Indice: ${index}, Valor: ${value}`)
-})
+nomes.forEach((nome, indice) => {
+    console.log(`Nome: ${nome}, Index: ${indice}`)
+});
 // 2. Verificar se todos os nomes possuem mais de 3 letras usando `every()` . 
-console.log(nomes.every((nome) => nome.length > 3))
+console.log(nomes.every((nome) => nome.length > 3 ))
+
 // 3. Ordenar os nomes em ordem alfabética.
 console.log(nomes.sort())
+
 // 4. Exibir o primeiro e o último nome da lista usando `.at()`.
-console.log(nomes.at(0))
-console.log(nomes.at(-1))
+console.log(nomes.at(0), nomes.at(-1))
+console.log(nomes[0], nomes[nomes.length - 1])
+
 // 5. Mostrar uma substring com os 3 primeiros caracteres de cada nome.
-const str = nomes.map((nome) => nome.substring(0,3)).join(',')
-console.log(str)
+console.log(nomes.map((nome) => nome.substring(0,3)))
+
 // 6. Reverter a lista e exibi-la.
 console.log(nomes.reverse())
+
 // 7. Exibir cada nome usando `for...of`.
-for (let values of nomes) {
-    console.log(values)
+for (let nome of nomes) {
+    console.log(nome)
 }
