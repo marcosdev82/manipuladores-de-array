7bcb6aaade11821b470e55ba7bbd515b0394935c
mikek182 <marcosvinicius@geekwork.com.br>
2025-06-11 17:34:32 -0300

    Exercício 2

--- Instance methods/exercícios/exercicio-2.js
+++ Instance methods/exercícios/exercicio-2.js
@@ -12,38 +12,21 @@ const grupo1 = [
 const grupo2 = [
   { nome: 'Bruna', idade: 22 },
   { nome: 'Diego', idade: 30 },
-  { nome: 'Marcos', idade: null },
+  { nome: 'Marcos', idade: null  },
 ];
-
-// // - Combinar dois arrays usando `concat`
-// let allgrupo = grupo1.concat(grupo2)
-// console.log(allgrupo)
-
-// // - Filtrar elementos com `filter`
-// const idade = allgrupo.filter((pessoa) => pessoa.idade !== null)
-// console.log(idade)
-
-// // - Verificar uma condição com `every`
-// const isNull = idade.every((idade) => idade.idade !== null )
-// console.log(isNull)
-
-// let novoGrupo = allgrupo.push({nome: 'Fabio', idade: 60})
-// console.log(novoGrupo)
-// console.log(allgrupo)
-
 
 // - Combinar dois arrays usando `concat`
 const grupo = grupo1.concat(grupo2)
 console.log(grupo)
 
 // - Filtrar elementos com `filter`
-const idadeNotNull = grupo.filter((pessoa) => pessoa.idade !== null )
-console.log(idadeNotNull)
+const notNull = grupo.filter((pessoa) => pessoa.idade === null )
+console.log(notNull)
 
 // - Verificar uma condição com `every`
-const allNotNull = grupo.every((pessoa) => pessoa.idade !== null )
-console.log(allNotNull)
+const allIdade = grupo.every((nome) => typeof nome.idade ===  "number")
+console.log(allIdade)
 
 // - Adicionar novos elementos com `push`
-grupo.push( { nome: 'Heitor', idade: 18 })
-console.log(grupo)
+const newArray = grupo.push({nome: 'Thiago', idade: 40})
+console.log(newArray, grupo)
