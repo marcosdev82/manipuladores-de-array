8c94e2858b5e2d7c8611f7a9e1cd301ce602c195
mikek182 <marcosvinicius@geekwork.com.br>
2025-06-12 17:24:31 -0300

    exercicio 10

--- Instance methods/exercícios/exercicio-10.js
+++ Instance methods/exercícios/exercicio-10.js
@@ -18,26 +18,27 @@
 // filter – Crie um novo array apenas com as notas maiores ou iguais a 7 (alunos aprovados).
 const notas = [8, 7.5, 9, 6, 5.5, 10, 4, 8.5, 6.5, 9.5];
 
-const result = notas.filter((nota) => nota >= 7)
-console.log('Alunos aprovados:', result); // Resultado esperado: [8, 7.5, 9, 10, 8.5, 9.5]
+// filter – Crie um novo array apenas com as notas maiores ou iguais a 7 (alunos aprovados).
+console.log(notas.filter((nota) => nota >= 7))
 
 // some – Verifique se algum aluno tirou nota abaixo de 5 (recuperação crítica).
-const notaAbaixoDeCinco = result.some((nota) => nota < 5)
-console.log('Algum aluno tirou nota abaixo de 5:', notaAbaixoDeCinco); // Resultado esperado: false
+console.log(notas.some((nota) => nota < 5 ))
 
 // every – Verifique se todos os alunos tiveram nota maior que 4 (ninguém zerou a prova).
-const notaMaiorQueQuatro = result.every((nota) => nota > 4)
-console.log('Todos os alunos tiveram nota maior que 4:', notaMaiorQueQuatro); // Resultado esperado: true
+console.log(notas.every((nota) => nota > 4))
+
 // includes – Verifique se algum aluno tirou nota 10.
-const notaDez = result.includes(10)
-console.log('Algum aluno tirou nota 10:', notaDez); // Resultado esperado: true
+console.log(notas.includes(10))
 
 // reduce – Calcule a média da turma.
-const media = notas.reduce((acc, curr) => acc + curr, 0) / notas.length
-console.log('Média da turma:', media.toFixed(2)); // Resultado esperado: 7.55
+const media = notas.reduce((acc, curr) => {
+    return acc + curr
+}, 0) 
+console.log(media / notas.length)
 
 // fill – Suponha que a nota do 4º aluno foi perdida. Substitua a nota da posição 3 por 0 usando .fill().
-let novoArray = [...notas]
-let arr = novoArray.fill(0, 3, 4)
-console.log('Novo array com a nota do 4º aluno substituída por 0:', arr); // Resultado esperado: [8, 7.5, 9, 0, 5.5, 10, 4, 8.5, 6.5, 9.5]
+console.log(notas.fill(0, 3,4))
 
+// filter – Crie um novo array apenas com as notas maiores ou iguais a 7 (alunos aprovados).
+
+console.log(notas.filter((nota) => nota >= 7))
