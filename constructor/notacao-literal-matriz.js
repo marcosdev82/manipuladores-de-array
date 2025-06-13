059307ba99bea299a52331894c60eacb27023a24
mikek182 <marcosvinicius@geekwork.com.br>
2025-06-13 17:54:56 -0300

    notação literal

--- constructor/notacao-literal-matriz.js
+++ constructor/notacao-literal-matriz.js
@@ -2,10 +2,10 @@ const fruits = ["Apple", "Banana"];
 
 console.log(fruits.length); // 2
 console.log(fruits[0]); // "Apple"
-console.log(Array('2').length)
+console.log(Array('2').length)  // inseri um valor na primeira posição do array [ '2' ]
 
 
-// Tres forma de transformar um string em array
+// Três forma de transformar uma string em array
 console.log(Array.from('marcos'))
 console.log([...'marcos'])
 console.log('marcos'.split(''))
