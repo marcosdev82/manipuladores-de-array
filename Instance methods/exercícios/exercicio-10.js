e5ed4e38287a58432a093a43f55a38101840add2
mikek182 <marcosvinicius@geekwork.com.br>
2025-06-23 09:26:01 -0300

    exercicio 10

--- Instance methods/exercícios/exercicio-10.js
+++ Instance methods/exercícios/exercicio-10.js
@@ -16,29 +16,31 @@
 
 // const notas = [8, 7.5, 9, 6, 5.5, 10, 4, 8.5, 6.5, 9.5];
 // filter – Crie um novo array apenas com as notas maiores ou iguais a 7 (alunos aprovados).
-const notas = [8, 7.5, 9, 6, 5.5, 10, 4, 8.5, 6.5, 9.5];
+const notas = [8, 7.5, 9, 6, 5.5, 10, 8.5, 6.5, 9.5];
 
-// filter – Crie um novo array apenas com as notas maiores ou iguais a 7 (alunos aprovados).
 console.log(notas.filter((nota) => nota >= 7))
 
 // some – Verifique se algum aluno tirou nota abaixo de 5 (recuperação crítica).
-console.log(notas.some((nota) => nota < 5 ))
+if(notas.some((nota) => nota < 5)) {
+    console.log('recuperação crítica')
+}
 
 // every – Verifique se todos os alunos tiveram nota maior que 4 (ninguém zerou a prova).
-console.log(notas.every((nota) => nota > 4))
+if(notas.every((nota) => nota > 4)) {
+    console.log('ninguém zerou a prova')
+}
 
 // includes – Verifique se algum aluno tirou nota 10.
 console.log(notas.includes(10))
 
 // reduce – Calcule a média da turma.
-const media = notas.reduce((acc, curr) => {
+
+let media = notas.reduce((acc, curr) => { 
     return acc + curr
-}, 0) 
-console.log(media / notas.length)
+}, 0) / notas.length
+
+console.log(media)
 
 // fill – Suponha que a nota do 4º aluno foi perdida. Substitua a nota da posição 3 por 0 usando .fill().
-console.log(notas.fill(0, 3,4))
-
-// filter – Crie um novo array apenas com as notas maiores ou iguais a 7 (alunos aprovados).
-
-console.log(notas.filter((nota) => nota >= 7))
+let novasNotas = notas.fill(0, 3, 4)
+console.log(novasNotas)
