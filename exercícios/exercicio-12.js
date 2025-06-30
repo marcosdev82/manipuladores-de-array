93a1f764f89c2bc9650226a4bea3abb7c0e91aea
mikek182 <marcosvinicius@geekwork.com.br>
2025-06-30 10:41:56 -0300

    exercicio 12

--- exercícios/exercicio-12.js
+++ exercícios/exercicio-12.js
@@ -49,29 +49,23 @@
 // console.log('Array em objeto:', emObjeto); // { a: 1, b: 2, c: 3 }
 
 
-// 1. Filter em array bidimensional
+// 1. Filter um array bidimensional onde o id for diferente de 2
 const dados = [
   [1, 'João'],
   [2, 'Maria'],
   [3, 'Carlos']
 ];
 
-// forma 1 de fazer
-const arrNumberFilter = dados.filter((pessoa) => pessoa[0] !== 2)
-console.log(arrNumberFilter)
-
-const arrNumberFilter2 = dados.filter(([id, nome]) => id !== 2)
-console.log(arrNumberFilter2)
+const isNotTwo = dados.filter((pesso) => pesso[0] !== 2)
+console.log(isNotTwo)
+const isNotTwo2 = dados.filter(([id, nome]) => id !== 2)
+console.log(isNotTwo2)
 
 // 2. Encontre a Maria usando o Array bidimencional
-const findPerson = dados.find((person) => person[1] === 'Maria')
-console.log(findPerson)
-
-const findPerson2 = dados.find(([id, nome]) => nome === 'Maria')
-console.log(findPerson2)
+const isMaria = dados.find(([id, pessoa]) => pessoa === 'Maria')
+console.log(isMaria)
 
 // 3. Conectando arrays - produtos a seus usuários
-
 const usuarios = [
   { id: 1, nome: 'Ana' },
   { id: 2, nome: 'Bruno' }
@@ -83,8 +77,25 @@ const produtos = [
   { nome: 'Fone', userId: 1 }
 ];
 
-const produtosComUsuarios = produtos.map(produto => {
-  const usuario = usuarios.find(u => u.id === produto.userId);
-  return { ...produto, usuario: usuario?.nome };
-});
-console.log(produtosComUsuarios)
+const productWithUser = produtos.map((produto) => {
+  const user = usuarios.find((user) => user.id === produto.userId)
+  return {...produto, 'usuario': user.nome}
+})
+console.log(productWithUser)
+
+// 4. Converter objeto em array
+const obj = { a: 1, b: 2, c: 3 };
+const arr = Object.entries(obj)
+console.log(typeof arr)
+
+// 5. Array de objetos com chave e valor separados
+
+const obj2 = { a: 1, b: 2, c: 3 };
+const arr2 = Object.entries(obj2).map(([key, value]) => ({ key, value }));
+console.log(typeof arr2, arr2);
+// [ { key: 'a', value: 1 }, { key: 'b', value: 2 }, { key: 'c', value: 3 } ]
+
+// 5. Converter array em objeto
+// const array = [['a', 1], ['b', 2], ['c', 3]];
+// const emObjeto = Object.fromEntries(array);
+// console.log('Array em objeto:', emObjeto); // { a: 1, b: 2, c: 3 }
