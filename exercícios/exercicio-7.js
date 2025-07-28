a7442eaec49d1ba0ac106461a00a09ecd80955b4
mikek182 <marcosvinicius@geekwork.com.br>
2025-07-28 11:32:04 -0300

    exercicio 7

--- exercícios/exercicio-7.js
+++ exercícios/exercicio-7.js
@@ -15,14 +15,11 @@ const usuarios = [
   { id: 5, nome: "Eduarda", ativo: false }
 ];
 
-// Encontre o primeiro usuário ativo usando find().
+
 console.log(usuarios.find((usuario) => usuario.ativo === true))
 
-// Encontre o índice do primeiro usuário inativo usando findIndex().
-console.log(usuarios.findIndex((usuario) => usuario.ativo === true))
+console.log(usuarios.findIndex((usuario) => usuario.ativo === false))
 
-// Encontre o último usuário ativo usando findLast().
 console.log(usuarios.findLast((usuario) => usuario.ativo === true))
 
-// Encontre o índice do último usuário inativo usando findLastIndex().
-console.log(usuarios.findLastIndex((usuario) => usuario.ativo === true))
+console.log(usuarios.findLastIndex((usuario) => usuario.ativo === false))
