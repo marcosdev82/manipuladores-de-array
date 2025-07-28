2a22993eea10b6d5714df64042e8c0e7fa11bc6b
mikek182 <marcosvinicius@geekwork.com.br>
2025-07-28 14:16:48 -0300

    exercicio 9

--- exercícios/exercicio-9.js
+++ exercícios/exercicio-9.js
@@ -34,46 +34,39 @@
 // }, {});
 // console.log('Contagem de letras:', contagemLetras); // Resultado esperado: { b: 1, a: 3, n: 2 }
 
-
 // Objetivo: Some todos os números do array.
-let num = [1, 2, 3, 4, 5];
 
-const soma1 = num.reduce((acc, cur) =>{
-   return acc + cur
-},0)
-console.log(soma1)
+const soma = [1, 2, 3, 4, 5];
+
+console.log(soma.reduce((acc, curr) => acc + curr, 0))
 
 // Exercício 2 – Contar ocorrências de uma palavra
-const respostas2 = ['sim', 'não', 'sim', 'sim', 'não', 'sim'];
-
-const ocorrencia = respostas2.reduce((acc, curr) => { // 4
-   return curr == 'sim' ? acc + 1 : acc;
-}, 0)
-console.log(ocorrencia)
+const respostas = ['sim', 'não', 'sim', 'sim', 'não', 'sim'];
+console.log(respostas.reduce((acc, palavra) => palavra === 'sim' ? acc + 1 : acc, 0))
 
 // Exercício 3 – Transformar array em objeto
 // Objetivo: Transforme o array em um objeto onde a chave é o número e o valor é seu quadrado.
-const quadrado = [1, 2, 3, 4];
-
-const obj = quadrado.reduce((obj, num) => { 
-   obj[num] = num * num
-   return obj
+const numeros2 = [1, 2, 3, 4];
+const objetoQuadrados = numeros2.reduce((obj, curr) => {
+   obj[curr] = curr * curr
+   return obj;
 }, {})
-
-console.log(obj)
+console.log(objetoQuadrados)
 
 // Exercício 4 – Flatten de array
 // Objetivo: Transforme um array de arrays em um único array.
 const arrays = [[1, 2], [3, 4], [5]];
 
-const unicArr = arrays.reduce((acc, curr) => acc.concat(curr), [])
-console.log(unicArr)
+const arraysInArray = arrays.reduce((acc, curr) => acc.concat(curr), [])
+console.log(arraysInArray)
 
 // Exercício 5 – Contar caracteres em uma string
 // Objetivo: Conte a quantidade de vezes que cada letra aparece.
 const palavra = 'banana';
-const totalCaracteres = [...palavra].reduce((acc, curr) => {
-   acc[curr] = (acc[curr] || 0) + 1 
-   return acc
-} , {})
-console.log(totalCaracteres)
+
+const contaLetras = [...palavra].reduce((obj, num) => {
+   obj[num] = (obj[num] || 0) + 1
+   return obj;
+}, {})
+console.log(contaLetras)
+
