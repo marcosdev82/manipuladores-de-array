e4f253f9069c616cb562d1ac971cfcb8e83d2ee6
mikek182 <marcosvinicius@geekwork.com.br>
2025-07-31 09:00:05 -0300

    exercicio 10

--- exercícios/exercicio-11.js
+++ exercícios/exercicio-11.js
@@ -33,33 +33,3 @@ const tarefas = [
 //   tarefas.pop();
 // }
 // console.log("Tarefas finais:", tarefas);
-
-// 1. Verifique se todas as tarefas foram concluídas
-const taskCompleted = tarefas.every((task) => task.concluida === true)
-console.log(taskCompleted)
-
-// 2. Encontre o índice da última tarefa não concluída
-const notTaskCompleted = tarefas.findLastIndex((task) => task.concluida === false)
-console.log(notTaskCompleted)
-
-// 3. Use reduceRight para criar uma string com os títulos das tarefas em ordem reversa
-const strReverse = tarefas.reduceRight((acc, curr) =>  acc + " | " + curr.titulo , "");
-console.log(strReverse)
-
-// 4. Verifique se 'tarefas' é um array
-const isArray = Array.isArray(tarefas)
-console.log(isArray)
-
-// 5. Crie uma nova lista de tamanho 5 e preencha com "Pendente"
-const listPendent = Array.from({ length: 5}).fill('pendente')
-console.log(listPendent)
-
-// 6. Remova a primeira tarefa e imprima
-const removeFirstTask = tarefas.shift()
-console.log(removeFirstTask)
-
-// 7. Use while para remover tarefas até que só sobrem 2
-while(tarefas.length > 2) {
-    tarefas.pop()
-}
-console.log(tarefas)
