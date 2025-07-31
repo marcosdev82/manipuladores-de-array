9140b947cbb6723b8bdb43e34df65af28110cba7
mikek182 <marcosvinicius@geekwork.com.br>
2025-07-31 10:18:26 -0300

    exercicio 11

--- exercícios/exercicio-11.js
+++ exercícios/exercicio-11.js
@@ -15,6 +15,9 @@ const tarefas = [
 
 // // 3. Use reduceRight para criar uma string com os títulos das tarefas em ordem reversa
 // const titulosReversos = tarefas.reduceRight((acc, tarefa) => acc + " | " + tarefa.titulo, "");
+// const orderTitle = tarefas.reduceRight((string, titulo) => {
+//     return  string + (string ? ' , ' : '') + titulo.titulo
+// },  '')
 // console.log("Títulos ao contrário:", titulosReversos);
 
 // // 4. Verifique se 'tarefas' é um array
@@ -33,3 +36,35 @@ const tarefas = [
 //   tarefas.pop();
 // }
 // console.log("Tarefas finais:", tarefas);
+
+// 1. Verifique se todas as tarefas foram concluídas
+const concluded = tarefas.every((task) => task.concluida === true)
+console.log(concluded)
+
+// 2. Encontre o índice da última tarefa não concluída
+const lasIndex = tarefas.findLastIndex((task) => task.concluida === true)
+console.log(lasIndex)
+
+// 3. Use reduceRight para criar uma string com os títulos das tarefas em ordem reversa
+const orderTitle = tarefas.reduceRight((string, titulo) => {
+    return  string + (string ? ' , ' : '') + titulo.titulo
+},  '')
+console.log(orderTitle)
+
+// 4. Verifique se 'tarefas' é um array
+console.log(Array.isArray(tarefas))
+
+// 5. Crie uma nova lista de tamanho 5 e preencha com "Pendente"
+const slot = new Array(5);
+console.log(slot.fill('pendente'))
+
+// 6. Remova a primeira tarefa e imprima
+const tarefaRemovida = tarefas.shift()
+console.log(tarefaRemovida)
+console.log(tarefas)
+
+// 7. Use while para remover tarefas até que só sobrem 2
+while(tarefas.length > 2) {
+    tarefas.pop()
+}
+console.log(tarefas)
