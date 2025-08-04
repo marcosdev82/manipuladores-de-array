3eba890b1bb669e706ceb7bb0db75a95f6ac808b
mikek182 <marcosvinicius@geekwork.com.br>
2025-08-04 14:05:54 -0300

    execicio 15

--- exercícios/exercicio-15.js
+++ exercícios/exercicio-15.js
@@ -1,5 +1,3 @@
- 
-
 // // 1. Use `flatMap` para transformar e achatar o array.
 // // transforme em [[1, 2], [2, 4], [3, 6]] e depois em [1, 2, 2, 4, 3, 6]
 // const numeros2 = [1, 2, 3];
@@ -68,6 +66,11 @@
 // const array = [['a', 1], ['b', 2], ['c', 3]];
 // console.log(Object.fromEntries(array))
 
+// console.log(array.reduce((obj, [key, value]) => {
+//     obj[key] = value
+//     return obj
+// }, {}))
+
 // //  8. Agrupar objetos por uma propriedade
 
 // const pessoas = [
@@ -75,6 +78,7 @@
 //     { nome: 'João', idade: 20 },
 //     { nome: 'Maria', idade: 30 }
 // ];
+//
 // const agrupado = pessoas.reduce((acc, pessoa) => {
 //     const chave = pessoa.idade;
 //     acc[chave] = acc[chave] || [];
@@ -87,6 +91,102 @@
 //   20: [{nome: 'Ana'...}, {nome: 'João'...}],
 //   30: [{nome: 'Maria'...}]
 // }
- 
 
- 
+// -------------------------------------------------
+
+
+// 1. Use `flatMap` para transformar e achatar o array.
+// transforme em [[1, 2], [2, 4], [3, 6]] e depois em [1, 2, 2, 4, 3, 6]
+const numeros2 = [1, 2, 3];
+
+const transforme = numeros2.flatMap((arr) => {
+    return [[arr, arr * 2]]
+})
+console.log(transforme)
+console.log(transforme.flat())
+
+// 2. Liste as entradas (chave e valor) do objeto abaixo.
+const produto = { nome: "Notebook", preco: 2500 };
+
+for ([chave, valor] of Object.entries(produto)) {
+    console.log(`${chave}: ${valor}`)
+}
+
+Object.entries(produto).forEach(([index, value]) => {
+    console.log(`${index}: ${value}`)
+});
+
+// 3. `flatMap` – Normalizar lista de pedidos com produtos**
+
+// Você tem uma lista de pedidos, e cada pedido tem múltiplos produtos. Crie um único array com todos os produtos de todos os pedidos, contendo também o ID do pedido.
+
+const pedidos = [
+    { id: 1, produtos: ["Mouse", "Teclado"] },
+    { id: 2, produtos: ["Monitor"] },
+    { id: 3, produtos: ["HD", "SSD"] }
+];
+
+const normalizar = pedidos.flatMap((pedido)=> {
+    return pedido.produtos.map((produto) => ({
+        pedidoId: pedido.id,
+        produto: produto
+    }))
+})
+console.log(normalizar)
+
+// 4. filtrar um array bidimensional onde o id for diferente de 2
+const dados = [
+    [1, 'João'],
+    [2, 'Maria'],
+    [3, 'Carlos']
+];
+
+console.log(dados.filter(([index,pessoa]) => index !== 2 ))
+
+// 5. Encontre a Maria usando o Array bidimencional
+console.log(dados.find(([index, nome]) => nome === 'Maria'))
+
+// 6. Conectando arrays - produtos a seus usuários
+const usuarios = [
+    { id: 1, nome: 'Ana' },
+    { id: 2, nome: 'Bruno' }
+];
+
+const produtos = [
+    { nome: 'Notebook', userId: 1 },
+    { nome: 'Celular', userId: 2 },
+    { nome: 'Fone', userId: 1 }
+];
+
+const userPedidos = produtos.map((produto) => {
+    const pessoa = usuarios.find((user) => produto.userId === user.id)
+    return {...produto, 'usuario': pessoa.nome}
+    // return pessoa;
+})
+console.log(userPedidos)
+
+// 7. Converter array em objeto
+const array = [['a', 1], ['b', 2], ['c', 3]];
+
+console.log(Object.fromEntries(array))
+
+console.log(array.reduce((obj, [key, value]) => {
+    obj[key] = value
+    return obj
+}, {}))
+
+//  8. Agrupar objetos por uma propriedade
+const pessoas = [
+    { nome: 'Ana', idade: 20 },
+    { nome: 'João', idade: 20 },
+    { nome: 'Maria', idade: 30 }
+];
+
+const agrupados = pessoas.reduce((obj, pessoa) =>{
+    const key = pessoa.idade
+    obj[key] = obj[key] || []
+    obj[key].push(pessoa)
+    return obj;
+}, {})
+console.log(agrupados)
+
