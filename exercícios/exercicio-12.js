0c567d495d8185beaf2c45f7660f5fe9ca4bc68f
mikek182 <marcosvinicius@geekwork.com.br>
2025-08-12 09:24:34 -0300

    Exercio 12

--- exercícios/exercicio-12.js
+++ exercícios/exercicio-12.js
@@ -1,8 +1,8 @@
 // // 1. Filter em array bidimensional onde o id for diferente de 2
 // const dados = [
-//   [1, 'João'],
-//   [2, 'Maria'],
-//   [3, 'Carlos']
+//   [1, 'João', '001'],
+//   [2, 'Maria', '002'],
+//   [3, 'Carlos', '003']
 // ];
 
 // const filtrado = dados.filter(([id, nome]) => id !== 2);
@@ -51,18 +51,14 @@
 
 // 1. filtrar um array bidimensional onde o id for diferente de 2
 const dados = [
-  [1, 'João'],
-  [2, 'Maria'],
-  [3, 'Carlos']
+  [1, 'João', '001'],
+  [2, 'Maria', '002'],
+  [3, 'Carlos', '003']
 ];
 
-console.log(dados.filter((pessoa, index) => pessoa[0] !== 2))
-
+console.log(dados.filter(([id, nome, codigo]) => codigo !== '002'))
 // 2. Encontre a Maria usando o Array bidimencional
-
-console.log(dados.find((pessoa) => pessoa[1] === 'Maria'))
-console.log(dados.find(([id, nome]) => nome === 'Maria'))
-
+console.log(dados.filter(([id, nome, codigo]) => nome == 'Maria'))
 
 // 3. Conectando arrays - produtos a seus usuários
 const usuarios = [
@@ -76,16 +72,27 @@ const produtos = [
   { nome: 'Fone', userId: 1 }
 ];
 
-const carrinho = produtos.map((produto) => {
-  const userProd = usuarios.find((usuario) => usuario.id === produto.userId)
-  return {...produto, 'usuario': userProd?.nome}
+const pedidos = produtos.map((produto) => {
+    const user = usuarios.find((user) => produto.userId === user.id)
+    return {...produto, 'usuario': user?.nome }
 })
-console.log(carrinho)
+console.log(pedidos)
 
 // 4. Converter objeto em array
-const obj = { a: 1, b: 2, c: 3 };
-console.log(Object.entries(obj))
+let obj = { a: 1, b: 2, c: 3 };
+
+const arr = Object.entries(obj)
+console.log(arr)
+obj = Object.fromEntries(arr)
+console.log(obj)
 
 // 5. Converter array em objeto
 const array = [['a', 1], ['b', 2], ['c', 3]];
 console.log(Object.fromEntries(array))
+console.log('--------- usando reduce --------')
+
+const arrFlat = array.reduce((obj, item)=> {
+  obj[item[0]] = item[1]
+  return obj
+}, {})
+console.log(arrFlat)
