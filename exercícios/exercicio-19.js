70f6280acad326204f8b219ceaf781ee3d231347
mikek182 <marcosvinicius@geekwork.com.br>
2025-08-16 10:49:05 -0300

    exercicio 19

--- exercícios/exercicio-19.js
+++ exercícios/exercicio-19.js
@@ -132,3 +132,58 @@
 // console.log(doMaisCaroAoMaisBarato);
 // ```
 // // ------------------------------------------------------------------
+
+
+const produtos = [
+    { id: 1, nome: 'Notebook', preco: 3500.5, estoque: 12 },
+    { id: 2, nome: 'Smartphone', preco: 2200.99, estoque: 0 },
+    { id: 3, nome: 'Monitor', preco: 1200.75, estoque: 7 },
+    { id: 4, nome: 'Teclado', preco: 150.4, estoque: 24 },
+    { id: 5, nome: 'Mouse', preco: 100.1, estoque: 30 },
+];
+
+// Exercício 1 — Usando `.some()`
+
+// **Verifique se existe algum produto com preço superior a R\$ 3000.**
+console.log(produtos.some((produto) => produto.preco > 3000))
+
+// Exercício 2 — Usando `break`
+
+// **Percorra o array com `for` e pare ao encontrar um produto com estoque 0. Mostre o nome.**
+for (let produto of produtos) {
+    if (produto.estoque == 0) {
+        console.log(produto.nome)
+        break
+    }
+}
+
+// ### ✅ Exercício 3 — Usando `.map()`
+
+// **Crie um novo array contendo apenas nome e preço formatado com `toFixed(2)` (em reais).**
+
+const listProdutos = produtos.map((produto) => {
+    return {'nome': produto.nome, 'preco': produto.preco.toFixed(2)}
+})
+console.log(listProdutos)
+
+
+// ### ✅ Exercício 4 — Usando `.sort()` por nome
+
+// **Ordene os produtos alfabeticamente pelo nome.**
+console.log(produtos.sort((a,b) => a.nome.localeCompare(b.nome)))
+
+// ### ✅ Exercício 5 — Usando `.sort()` por valor
+
+// **Ordene os produtos do mais barato para o mais caro.**
+console.log(produtos.sort((a,b) => a.preco - b.preco))
+
+// ### ✅ Exercício 6 — Usando `.toReversed()`
+
+// **Crie uma cópia do array de produtos na ordem inversa (sem modificar o original).**
+console.log(produtos.toReversed())
+
+// ### ✅ Exercício 7 — Usando `.splice()`
+
+// **Remova os dois últimos produtos do array original e exiba os removidos.**
+console.log(produtos.splice(3, 5))
+console.log(produtos)
