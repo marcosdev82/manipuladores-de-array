3359c43698ccc8c2d0eb590eb35bd13ead043117
mikek182 <marcosvinicius@geekwork.com.br>
2025-08-27 17:27:59 -0300

    exercicio 3

--- typescript/exercicio-3.ts
+++ typescript/exercicio-3.ts
@@ -168,20 +168,22 @@ interface Usuario {
     idade: number;
 }
 
-function boasVindas(usuario: Usuario): string { 
-    return `Bem-vindo(a), ${usuario.nome}! Seu email é ${usuario.email} e você tem ${usuario.idade} anos.`;
+function boasVindas(usuario: Usuario): string {
+    return `Bem-vindo(a), ${usuario.nome}! Seu email é ${usuario.email} e você tem ${usuario.idade} anos.`
 }
 
-const user: Usuario = {
-    nome: 'marcos tavares',
-    email: 'marcos@teste.com',
+const usuario: Usuario = {
+    nome: 'marcos',
+    email: 'marcos@teste.com.br',
     idade: 42,
 }
 
-console.log(boasVindas(user))
+console.log(boasVindas(usuario))
 
 
-// ### **Exercício 2: Interface `Produto` e carrinho de compras**
+
+// ### ✅ **Exercício 2: Interface `Produto` e carrinho de compras**
+
 
 // { nome: "Camisa", preco: 50 },
 // { nome: "Tênis", preco: 200 },
@@ -195,17 +197,17 @@ interface Produto {
     preco: number;
 }
 
-function calcularTotal(produtos: Produto[]): number {
-    return produtos.reduce((total, produto) => total + produto.preco, 0);
+function calcularTotal(produto: Produto[] ): number{
+    return produto.reduce((total, produto) => total + produto.preco, 0)
 }
 
-const produtos: Produto[] = [
+const produto: Produto[] = [
     { nome: "Camisa", preco: 50 },
     { nome: "Tênis", preco: 200 },
-    { nome: "Boné", preco: 30 },
+    { nome: "Boné", preco: 30 },  
 ]
 
-console.log(`Valor total no carrinho: R$ ${calcularTotal(produtos).toFixed(2)}`)
+console.log(`Total da compra: R$ ${calcularTotal(produto)}`)
 
 // ### **Exercício 3: Interface `Animal` com diferentes tipos**
 
@@ -218,129 +220,29 @@ console.log(`Valor total no carrinho: R$ ${calcularTotal(produtos).toFixed(2)}`)
 
 interface Animal {
     nome: string;
-    tipo: "mamífero" | "ave" | "réptil";
-    emitirSom(): void;
+    tipo:  "mamifero" | "ave" | "repitil";
+    emitirSom(): void; 
 }
-
-const cachorro: Animal = {
-    nome: "Totó",
-    tipo: "mamífero",
-    emitirSom(){
-        console.log('Latir')
-    }
-}
-console.log(cachorro.emitirSom())
-console.log(cachorro)
 
 const gato: Animal = {
-    nome: "Mingal",
-    tipo: "mamífero",
+    nome: 'Mingal',
+    tipo: 'mamifero', 
     emitirSom() {
-        console.log('Miau')
-    },
+        console.log('miau')
+    }
 }
-console.log(gato.emitirSom())
-console.log(gato)
 
-const papagaio: Animal = {
-    nome: "Loro",
+const galo: Animal = {
+    nome: "Frangolino",
     tipo: "ave",
     emitirSom() {
-        console.log('Currupaco')
-    },
-}
-console.log(papagaio.emitirSom())
-console.log(papagaio)
-
-// ### **Exercício 4: Interface com herança (`Funcionario` estende `Pessoa`)**
-
-// nome, idade
-// saida:  console.log(`${funcionario.nome}, ${funcionario.cargo}, R$ ${funcionario.salario}`);
-
-// interface Pessoa {
-//     nome: string;
-//     idade: number;
-// }
-
-// class Funcionario implements Pessoa {
-//     nome: string;
-//     idade: number;
-//     private salario: number;
-//     private cargo: string;
-
-//     constructor(nome: string, idade: number, cargo: string, salario: number) {
-//         this.setSalario(salario)
-//         this.setNome(nome);
-//         this.setCargo(cargo)
-//         this.setIdade(idade)
-//     }
-
-//     setNome(nome: string): void {
-//         if (nome.trim().length < 2) {
-//             throw new Error("Nome muito curto");
-//         }
-//         this.nome = nome;
-//     }
-
-//     setIdade(idade: number): void {
-//         this.idade = idade
-//     }
-
-//     setCargo(cargo: string): void {
-//         this.cargo = cargo;
-//     }
-
-//     setSalario(valor: number): void {
-//         this.salario = valor
-//     }
-
-//     getFuncionario(): string {
-//         return `${this.nome}, ${this.idade}, ${this.cargo}, Salário R$ ${this.salario}`;
-//     }
-
-// }
-
-// console.log(new Funcionario('Marcos', 42, 'Programador', 6000).getFuncionario())
-
-interface Pessoa {
-    nome: string;
-    idade: number;
+        console.log('Cocorico')
+    }
 }
 
-interface Funcionario extends Pessoa {
-    cargo: string;
-    salario: number;
-}
+console.log(gato.emitirSom())
+console.log(galo.emitirSom())
 
-const funcionario: Funcionario = {
-    nome: "Marcos Tavares",
-    idade: 42,
-    cargo: "Programador",
-    salario: 6000
-}
-console.log(`${funcionario.nome}, ${funcionario.idade}, ${funcionario.cargo}, Salário R$ ${funcionario.salario}`);
 
-// ### **Exercício 5: Interface `Form` com validação**
+// export default 1
 
-// nome, email, senha
-
-// saida
-// console.log("Formulário válido?", validarForm(formValido));
-
-interface Form {
-    nome: string;
-    email: string;
-    senha: string;
-}
-
-function validarForm(form: Form): boolean {
-    return form.nome.length > 0 && form.email.includes('@') && form.senha.length >= 6
-}
-
-const form: Form = {
-    nome: 'Marcos Tavares',
-    email: 'marcos@teste.com',
-    senha: '123654',
-}
-
-console.log(`Formulário válido?`, validarForm(form))
